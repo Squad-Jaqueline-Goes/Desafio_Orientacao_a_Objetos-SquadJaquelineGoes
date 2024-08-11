f638333a39b8c3b7d1cc906d90c4ee550f017d8d
Ana Carolyne <111382055+anadevti@users.noreply.github.com>
2024-08-11 16:43:50 -0300

    Update autor.py

--- classes/autor.py
+++ classes/autor.py
@@ -1,3 +1,31 @@
 class Autor:
-    def __init__(self):
-        pass
+    def __init__(self, nome, nacionalidade):
+        self._nome = nome
+        self._nacionalidade = nacionalidade
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        if not nome or nome == '':
+            raise ValueError('O Autor deve possuir um Nome')
+        if not isinstance(nome, str):
+            raise ValueError('Nome deve ser uma string')
+        self._nome = nome
+
+    @property
+    def nacionalidade(self):
+        return self._nacionalidade
+
+    @nacionalidade.setter
+    def nacionalidade(self, nacionalidade):
+        if not nacionalidade or nacionalidade == '':
+            raise ValueError('O Autor deve possuir uma Nacionalidade')
+        if not isinstance(nacionalidade, str):
+            raise ValueError('Nacionalidade deve ser uma string')
+        self._nacionalidade = nacionalidade
+
+    def __str__(self):
+        return f"{self.nome} ({self.nacionalidade})"
