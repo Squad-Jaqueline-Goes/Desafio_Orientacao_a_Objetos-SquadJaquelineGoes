e34ceecf209c8c9c3d94965d642be8cc68a06d86
Larissa Albuquerque <106053968+lalbuquerques@users.noreply.github.com>
2024-08-12 12:21:33 -0300

    Update usuario.py

--- classes/usuario.py
+++ classes/usuario.py
@@ -1,3 +1,9 @@
-class Usuario:
-    def __init__(self):
-        pass
+class Usuario(Pessoa):
+    def __init__(self, nome, telefone, nacionalidade):
+        super().__init__(nome, telefone, nacionalidade)
+
+    def exibir_dados(self):
+        return f"Nome: {self.nome}, Telefone: {self.telefone}, Nacionalidade: {self.nacionalidade}"
+
+    def __str__(self):
+        return self.exibir_dados()
