11d071d9a46eb27152e4df80253ddd7ce202e3a1
Juliana Portela <julianaportehla@hotmail.com>
2024-08-15 12:28:06 -0300

    Adicionando metodo renovar emprestimo

--- classes/emprestimo.py
+++ classes/emprestimo.py
@@ -3,7 +3,7 @@ from classes.exemplar import Exemplar
 
 
 class Emprestimo:
-    def __init__(self, data_emprestimo, data_devolucao, usuario, exemplar):
+    def __init__(self, data_emprestimo, data_devolucao, usuario, exemplar, renovar_emprestimo):
         if not isinstance(usuario):
             raise ValueError("usuario deve ser uma instância da classe Usuario")
         if not isinstance(exemplar):
@@ -13,6 +13,7 @@ class Emprestimo:
         self.__data_devolucao = data_devolucao
         self.__usuario = usuario
         self.__exemplar = exemplar
+        self.__renovar_empresatimo = renovar_emprestimo
 
 
     """
@@ -36,6 +37,10 @@ class Emprestimo:
     @property
     def exemplar(self):
         return self.__exemplar
+    
+    @property
+    def renovar_emprestimo(self):
+        return self.__renovar_empresatimo
 
 
     """
@@ -62,3 +67,17 @@ class Emprestimo:
             self.__exemplar.atualizar_estado("disponível")
             return True
         raise Exception("O exemplar já foi devolvido ou não foi emprestado")
+    
+    """
+    Renova o empréstimo, estendendo a data de devolução.
+    A renovação só é possível se o exemplar estiver atualmente emprestado.
+    """
+
+    def renovar_emprestimo(self, dias_adicionais):
+        if self.__exemplar.get_informacoes()['estado'] == "emprestado":
+            data_ordinal = self.__data_devolucao.toordinal()
+            novo_data_ordinal = data_ordinal + dias_adicionais
+            nova_data_devolucao = self.__data_devolucao.fromordinal(novo_data_ordinal)
+            self.__data_devolucao = nova_data_devolucao
+            return True
+        raise Exception("Não é possível renovar o empréstimo porque o exemplar não está emprestado.")
